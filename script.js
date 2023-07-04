bcee1f00eb49f46d92b4511b09133301022ac053
luizh3nrique <luizh3nrique00@gmail.com>
2023-07-04 12:07:27 -0300

    Signed-off-by: luizh3nrique <luizh3nrique00@gmail.com>

---
+++ script.js
@@ -0,0 +1,21 @@
+function calcular() {
+    const inputValor = document.querySelector('#valor').value;
+    const inputPorcentagem = document.querySelector('#porcentagem').value;
+    const resultado = document.querySelector('#resultado');
+
+    const valorResultado = inputValor * inputPorcentagem;
+
+    resultado.innerHTML = `${valorResultado.toFixed(2)}`;
+}
+
+
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode === 13) {
+        calcular();
+    }
+})
+
+document.querySelector('.clear').addEventListener('click', () => {
+    const inputValor = document.querySelector('#valor').value = '';
+    const resultado = document.querySelector('#resultado').innerHTML = '00.00';
+})
